1cb0e9f175314c60a8463d3957b06c80a5456b0e
Wim Bervoets <wim.bervoets@pxl.be>
2025-12-01 23:55:06 +0100

    Add project files.

---
+++ PieShop.App/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using PieShop.App.Views;
+
+namespace PieShop.App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(PieDetailView), typeof(PieDetailView));
+        }
+    }
+}
---
+++ PieShop.App/ViewModels/PieOverviewViewModel.cs
@@ -0,0 +1,83 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using PieShop.App.Message;
+using PieShop.App.Models;
+using PieShop.App.Services;
+using System.Collections.ObjectModel;
+
+
+namespace PieShop.App.ViewModels
+{
+    public partial class PieOverviewViewModel : ObservableObject, 
+        IRecipient<PieCreatedMessage>, IRecipient<PieUpdatedMessage>
+    {
+        public ObservableCollection<Pie> Pies { get; private set; } = new ObservableCollection<Pie>();
+
+        private IPieRepository _repository;
+        private readonly INavigationService _navigation;
+
+        public PieOverviewViewModel(IPieRepository pieRepository, INavigationService navigation, IMessenger messenger) 
+        {
+            _repository = pieRepository;
+            _navigation = navigation;
+            
+            messenger.Register<PieCreatedMessage>(this);
+            messenger.Register<PieUpdatedMessage>(this);
+        }
+
+        [RelayCommand]
+        private void OnAddPieToCart(Pie pie)
+        {
+            //TODO:
+            //Add Pie to CartRepository
+            //Show Toast
+        }
+
+        [RelayCommand]
+        private async Task OnPieSelected(Pie pie)
+        {
+            await _navigation.GoToAsync("PieDetailView", new Dictionary<string, object> { { "selectedPie", pie } });
+        }
+
+        [RelayCommand]
+        public async Task OnLoad()
+        {
+            try
+            {
+                this.Pies.Clear();
+                foreach (Pie pie in await _repository.GetAllPies())
+                {
+                    this.Pies.Add(pie);
+                }
+            }
+            catch (Exception ex)
+            {
+                await Toast.Make(ex.Message).Show();
+            }
+        }
+
+        [RelayCommand]
+        public async Task OnAdd()
+        {
+            await _navigation.GoToAsync("PieDetailView");
+        }
+
+        public void Receive(PieCreatedMessage message)
+        {
+            this.Pies.Add(message.Value);
+        }
+
+        public void Receive(PieUpdatedMessage message)
+        {
+            var updatedPie = message.Value;
+            var existingPie = this.Pies.FirstOrDefault(p => p.Id == updatedPie.Id);
+
+            if (existingPie is null)
+                return;
+
+            this.Pies[this.Pies.IndexOf(existingPie)] = updatedPie;
+        }
+    }
+}
